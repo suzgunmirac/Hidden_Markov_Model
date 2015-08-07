93ca56c821c3eeea489ac8181d8568d1b652f929
suzgunmirac <suzmuh.16@robcol.k12.tr>
2015-08-07 12:45:28 +0300

    first commit

---
+++ HiddenMarkovModelTrain/HiddenMarkovModelTrain/HMM.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenMarkovModelTrain
+{
+    class HMM
+    {
+        public int numStates; // number of states
+
+        public int numObservations; // size of the observations
+
+        public double [] prior; // initial states (prior)
+
+        public double [,] A; // transitionMatrix
+
+        public double [,] B; // emissionMatrix
+
+        /** Constructor
+         * @ param numStates
+         * @ param numObservations
+         **/
+        public HMM(int numStates, int numObservations)
+        {
+            prior = new double[numStates];
+
+            A = new double[numStates, numStates];
+            B = new double[numStates, numObservations];
+        }
+
+
+        /** Baum-Welch Training 
+         * @param data : the training set
+         * @param nSteps: the number of steps
+         **/
+        public void baumWelchTrain (int [] data, int nSteps)
+        {
+            int time_duration = data.Length;
+
+            double [,] alpha = new double [numStates, numStates];
+            double[,] beta = new double[numStates, numObservations];
+            double[] a0 = new double[numStates];
+
+            double[,] fwd;
+            double[,] bwd;
+
+            for (int step = 0; step<nSteps; step++){
+
+                //calculate the foward and backward variables on the model
+
+                fwd = forwardCalc (data);
+                bwd = backwardCalc (data);
+
+                // RE-ESTIMATION
+
+                // re-estimation of the prior=a0 (initial state) probabilities
+                for (int i = 0; i<numStates; i++){
+                    a0 [i] = gamma (i, 0, data, fwd, bwd);
+                }
+
+                // re-estimation of the the transtition prob
+                for (int i = 0; i<numStates; i++){
+                    for (int j = 0; j<numStates; j++){
+                        double numerator = 0;
+                        double denominator = 0;
+
+                        for (int t = 0; t<time_duration; t++){
+                            numerator += probCalc (t, i, j, data, fwd, bwd);
+                            denominator += gamma (i, t, data, fwd, bwd);
+                        }
+                        alpha [i, j] = divide(numerator, denominator);
+                    }
+                }
+
+
+                // re-estimaton of emission prob
+
+                for (int i = 0; i<numStates; i++){
+                    for (int k = 0; k<numObservations; k++){
+
+                        double numerator = 0;
+                        double denominator = 0;
+
+                        for (int t = 0; t<time_duration; t++){
+                            double e = gamma (i, t, data, fwd, bwd);
+
+                            // if data[t] is equal to k, then multiply e by 1, otherwise 0
+                            numerator += e * (data[t] == k ? 1 : 0); 
+                            denominator += e;
+                        }
+
+                        beta [i, k] = divide(numerator, denominator);
+                    }
+                }
+
+                prior = a0;
+                A = alpha;
+                B = beta;
+            }
+        }
+
+
+        /** Algorithm for Forward-Variable(s) Calculatiın at the state i, time t
+         * @param data -> integer olarak geliyor
+         * @return an array, fwd, that contains the forward-variables
+         * multi-dimens olduguna dikkat et!!! 
+         **/
+        public double [, ] forwardCalc (int [] data)
+        {
+            int time_duration = data.Length;
+
+            double[,] fwd = new double[numStates, time_duration];
+
+            // at time t = 0
+            // initialization of the initial prob states
+            for (int i = 0; i < numStates; i++)
+            {
+                fwd[i, 0] = prior[i] * B[i, data[0]];
+            }
+
+            // induction step
+            for (int t = 0; t<time_duration - 1; t++) // 0 <= t <= T-2
+            {
+                for (int j = 0; j < numStates; j++)
+                {
+                    fwd[j, t + 1] = 0.0; // ne olur ne olmaz diye 0 degil de 0.0 yaptım
+
+                    for (int i = 0; i < numStates; i++)
+                    {
+                        fwd[j, t + 1] += (fwd[i, t] * A[i, t]);
+                    }
+
+                    fwd[j, t + 1] *= B[j, data[t + 1]];
+                }
+            }
+
+            return fwd;
+        }
+
+        /** Algorith m for the Backward-Variables(s) Calculation at the state i, time t
+         * @ param data -> int alıyor
+         * @ return an array, bwd, that contains the backward-variables
+         * multi-dimension olduguna dikkat et
+         **/
+        public double [,] backwardCalc (int [] data)
+        {
+            int time_duration = data.Length;
+
+            double[,] bwd = new double[numStates, time_duration];
+
+            // at time t = 0 -> daha doğrusu t = t (amma velakin beta oldugundan t = 0)
+            // initialization 
+            for (int i = 0; i < numStates; i++)
+            {
+                bwd[i, time_duration - 1] = 1; // hepsi 1
+            }
+
+            // induction step
+            for (int t = time_duration - 2; t >= 0; t--)
+            {
+                for (int i = 0; i < numStates; i++)
+                {
+                    bwd[i, t] = 0.0; // ne olur ne olmaz diye 0 degil de 0.0 yaptım
+
+                    for (int j = 0; j < numStates; j++)
+                    {
+                        bwd[i, t] += (bwd[j, t + 1] * A[i, j] * B[j, data[t + 1]]);
+                    }
+                }
+            }
+
+            return bwd;
+        }
+
+        /** Calculates Gamma
+         * @ param"s": i ,t data, fwd, bwd
+         * @return gamma
+         **/
+        public double gamma (int i, int t, int [] data, double [,] fwd, double [,] bwd)
+        {
+            double numerator = fwd[i, t] * bwd[i, t];
+            double denominator = 0.0; // ne olur ne olmaz diye... = 0.0
+
+            for (int j = 0; j < numStates; j++)
+            {
+                denominator += (fwd[j, t] * bwd[j, t]);
+            }
+
+            return divide(numerator, denominator);
+        }
+
+
+        /** Calculates the probability p
+         * @ param t: time
+         * @ param i: s_i
+         * @ param j: s_j
+         * @ param data: data (Obs sequence)
+         * @ param fwd: forward
+         * @ param bwd: backward
+         * @ return probabiliy p
+         **/
+        double probCalc (int t, int i, int j, int [] data, double [, ] fwd, double [,] bwd)
+        {
+            double numerator;
+            double denominator = 0;
+
+            int time_duration = data.Length;
+
+            // if t == T -1 -> num = fwd(i,t) * A[i,j])
+            // else num = fwd(i,t) * A[i,j] * B [j, data[t+1] * bwd[j, t+1]
+            numerator = (t == time_duration -1 ? (fwd[i,t] * A[i,j]) : (fwd[i,t] * A[i,j] * B [j, data[t+1]] * bwd[j, t+1]));
+
+            //denom calc
+            for (int k = 0; k<numStates; k++){
+                denominator += (fwd[k,t] * bwd[k,t]);
+            }
+
+            return divide(numerator, denominator);
+        }
+
+
+        /**Divides two double numbers
+         * @ param numerator
+         * @ oaram denominatr
+         * @return num / denum -> float
+         **/
+        double divide (double num, double denum)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return num / denum;
+            }
+        }
+    }
+}
---
+++ HiddenMarkovModelTrain/HiddenMarkovModelTrain/logHMM.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenMarkovModelTrain
+{
+    class logHMM
+    {
+        public int numStates; // number of states
+
+        public int numObservations; // size of the observations
+
+        public double[] prior; // initial states (prior)
+
+        public double[,] A; // transitionMatrix
+
+        public double[,] B; // emissionMatrix
+
+
+        public double[,] fwd; // forward-vars
+        public double[,] bwd; // backward-vars
+
+        public double log_prob_forward;
+
+        public int time_duration;
+
+        /** Constructor
+         * @ param numStates
+         * @ param numObservations
+         **/
+        public logHMM(int numStates, int numObservations)
+        {
+            prior = new double[numStates];
+
+            A = new double[numStates, numStates];
+            B = new double[numStates, numObservations];
+        }
+
+
+        /** Baum-Welch Training 
+         * @param data : the training set
+         * @param nSteps: the number of steps
+         **/
+        public void baumWelchTrain(int[] data, int nSteps)
+        {
+            time_duration = data.Length;
+
+            double [,] gamma = new double [numStates, time_duration];
+
+            double[,] alpha_num = new double[numStates, numStates]; // new A (transition matrix)
+            double [] alpha_denom = new double [numStates];
+
+            double[,] beta_num = new double[numStates, numObservations]; // new B (emission matrix)
+            double [,] beta_denom = new double [numStates, numObservations];
+
+            double[] a0_num = new double[numStates]; // new prior 
+            double[] a0_denom = new double[numStates]; 
+
+
+
+            //calculate the foward and backward variables on the model
+
+            fwd = forwardCalc(data);
+            bwd = backwardCalc(data);
+
+            /** RE-ESTIMATION and UPDATE **/
+
+            // re-estimation of the prior=a0 (initial state) probabilities and UPDATE
+            for (int i = 0; i < numStates; i++)
+            {
+                a0_num[i] = fwd[0, i] + bwd[0, i];
+
+                for (int j = 0; j<numStates; j++){
+                    double gamma_t_j = fwd[0, i] + bwd [0, i];
+                    a0_denom [i] = log_Sum (a0_denom[i], gamma_t_j);
+                }
+
+                // Update prior with a0:
+                // which means prior = a0;
+                prior[i] = a0_num[i] - a0_denom[i];
+            }
+
+            // re-estimation of the the transtition prob
+            for (int t = 0; t<time_duration -2; t++){
+
+                for (int i=0; i<numStates; i++){
+                    alpha_denom[i] = double.NaN;
+
+                    for (int j = 0; j<numStates; j++){
+                        alpha_num[i, j] = double.NaN;
+                        
+
+                        double xi = fwd[t, i] + A[i, j] + B[j, data[t + 1]] + bwd[t + 1, j] - totalNumberOfTransCalc (t, i);
+                        alpha_num [i,j] = log_Sum (alpha_num [i,j], xi);
+                        alpha_denom[i] = log_Sum (alpha_denom[i], xi);
+                    }
+                }
+            }
+
+            // update A with alpha: A = alpha
+
+            for (int i = 0; i<numStates; i++){
+                
+                for (int j = 0; j<numStates; j++){
+
+                    A [i, j] = alpha_num[i,j] - alpha_denom[i]; 
+                }
+            }
+
+            // re-estimaton of emission prob 
+
+            for (int j = 0; j<numStates; j++){
+
+                for (int k = 0; k<numObservations; k++){
+                    beta_num[j, k] = double.NaN;
+                    beta_denom[j, k] = double.NaN;
+
+                    for (int t = 0; t < time_duration; t++)
+                    {
+                        double gamma_t_j = fwd[t, j] + bwd[t, j] - totalNumberOfTransCalc (t, j);
+
+                        // if data[t] is equal to k, then multiply gamma_t_j by 1, otherwise 0
+
+                        if (data[t] == k ){
+                            beta_num[j,k] = log_Sum (beta_num[j,k], gamma_t_j);
+                        }
+                        beta_denom[j, k] = log_Sum(beta_denom[j, k], gamma_t_j);
+
+                    }
+
+                    // UPDATE B [j,k] with beta [j,k]
+                    B[j, k] = beta_num[j, k] - beta_denom[j, k]; 
+                    
+                }
+            }
+
+        }
+
+
+        /** Algorithm for Forward-Variable(s) Calculatiın at the state i, time t
+         * @param data -> integer olarak geliyor
+         * @return an array, fwd, that contains the forward-variables
+         * multi-dimens olduguna dikkat et!!! 
+         **/
+        public double[,] forwardCalc(int[] data)
+        {
+            int time_duration = data.Length;
+
+            double[,] fwd = new double[time_duration, numStates];
+
+            for (int t = 0; t < time_duration; t++)
+            {
+                for (int i = 0; i < numStates; i++)
+                {
+                    fwd[t, i] = double.NegativeInfinity;
+                }
+            }
+
+            // at time t = 0
+            // initialization of the initial prob states
+            for (int i = 0; i < numStates; i++)
+            {
+                fwd[0, i] = prior[i] + B[i, data[0]];
+            }
+
+            // induction step
+            for (int t = 0; t < time_duration - 1; t++) // 0 <= t <= T-2
+            {
+                for (int j = 0; j < numStates; j++)
+                {
+                    for (int i = 0; i < numStates; i++)
+                    {
+                        double prob = fwd[t, i] + A[i, j] + B[j, data[t + 1]];
+                        fwd[t + 1, j] = log_Sum(fwd[t + 1, j], prob);
+                    }
+                }
+            }
+
+            log_prob_forward = double.NaN;
+
+            for (int i = 0; i < numStates; i++)
+            {
+                log_prob_forward = log_Sum(log_prob_forward, fwd[time_duration - 1, i]);
+            }
+
+            return fwd;
+        }
+
+        /** Algorith m for the Backward-Variables(s) Calculation at the state i, time t
+         * @ param data -> int alıyor
+         * @ return an array, bwd, that contains the backward-variables
+         * multi-dimension olduguna dikkat et
+         **/
+        public double[,] backwardCalc(int[] data)
+        {
+            int time_duration = data.Length;
+
+            double[,] bwd = new double[time_duration, numStates];
+
+            for (int t = 0; t < time_duration; t++)
+            {
+                for (int i = 0; i < numStates; i++)
+                {
+                    bwd[t, i] = double.NegativeInfinity;
+                }
+            }
+
+            // at time t = 0 -> daha doğrusu t = t (amma velakin beta oldugundan t = 0)
+            // initialization 
+            for (int i = 0; i < numStates; i++)
+            {
+                bwd[time_duration - 1, i] = 0; // log (1) = 0; -> normalde 1 olacakti ancak simdi 0 olacak log'dan dolayi
+            }
+
+            // induction step
+            for (int t = time_duration - 2; t >= 0; t--)
+            {
+                for (int i = 0; i < numStates; i++)
+                {
+                    for (int j = 0; j < numStates; j++)
+                    {
+                        double prob = A[i, j] + B[j, data[t + 1]] + bwd[t + 1, j];
+                        bwd[t, i] += log_Sum(bwd[t,i], prob);
+                    }
+                }
+            }
+
+            return bwd;
+        }
+
+        double log_Sum (double p1, double p2)
+        {
+            if (p1 > p2)
+            {
+                return p1 + Math.Log(Math.Exp(p2 - p1));
+            }
+            else
+            {
+                return p2 + Math.Log(Math.Exp(p1 - p2));
+            }
+        }
+
+        double totalNumberOfTransCalc (int t, int i)
+        {
+            double total_trans_num = fwd[t, i] + bwd[t, i];
+            double total_trans_denom = double.NaN;
+
+            for (int j = 0; j < numStates; j++)
+            {
+                total_trans_denom = log_Sum(total_trans_denom, (fwd[t, j] + bwd[t, j]));
+            }
+
+            return total_trans_num - total_trans_denom;
+        }
+
+    }
+}
